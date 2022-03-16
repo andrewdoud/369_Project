01272ab104f561ec82c109531e003e439be0bddd
andrewdoud <andrewxdoud@gmail.com>
2022-03-16 10:00:03 -0700

    preprocessed wine data

--- src/main/scala/DataPreprocess.scala
+++ src/main/scala/DataPreprocess.scala
@@ -1,14 +1,8 @@
-import org.apache.spark.SparkContext._
 
-import scala.io._
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd._
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
 
-import scala.collection._
 import scala.collection.immutable.HashMap
-import scala.math.Ordered.orderingToOrdered
 
 object DataPreprocess {
     def main(args: Array[String]) {
@@ -22,10 +16,10 @@ object DataPreprocess {
             "Unknown"->"1", "formerly smoked"->"2", "smokes"->"3", "Urban"->"0", "Rural"->"1")
 
         // id,gender,age,hypertension,heart_disease,ever_married,work_type,Residence_type,avg_glucose_level,bmi,smoking_status,stroke
-        val lines = sc.textFile("src/main/scala/healthcare-dataset-stroke-data.csv").map(x => x.split(",")).filter(_.head != "id").filter(x => x(9) != "N/A")
-        val linesEncoded = lines.map(x => (x(0).toInt, encodingMap(x(1)).toDouble, x(2).toDouble, x(3).toDouble,
-                                            x(4).toDouble, encodingMap(x(5)).toDouble, encodingMap(x(6)).toDouble, encodingMap(x(7)).toDouble,
-                                            x(8).toDouble, x(9).toDouble, encodingMap(x(10)).toDouble, x(11).toInt))
+        val lines = sc.textFile("src/main/scala/winequality-white.csv").map(x => x.split(";")).filter(_.head != "\"fixed acidity\"")
+        val linesEncoded = lines.map(x => x.map(y => y.toDouble)).map(x => (x(0), x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8), x(9), x(10), x(11)))
+
+        val col1Max = linesEncoded.map(x => x._1).max()
         val col2Max = linesEncoded.map(x => x._2).max()
         val col3Max = linesEncoded.map(x => x._3).max()
         val col4Max = linesEncoded.map(x => x._4).max()
@@ -36,7 +30,9 @@ object DataPreprocess {
         val col9Max = linesEncoded.map(x => x._9).max()
         val col10Max = linesEncoded.map(x => x._10).max()
         val col11Max = linesEncoded.map(x => x._11).max()
-        val linesScaled = linesEncoded.map(x => (x._1, x._2/col2Max, x._3/col3Max, x._4/col4Max, x._5/col5Max, x._6/col6Max, x._7/col7Max, x._8/col8Max, x._9/col9Max, x._10/col10Max, x._11/col11Max, x._12))
+
+        val linesScaled = linesEncoded.map(x => (x._1/col1Max, x._2/col2Max, x._3/col3Max, x._4/col4Max, x._5/col5Max,
+            x._6/col6Max, x._7/col7Max, x._8/col8Max, x._9/col9Max, x._10/col10Max, x._11/col11Max, x._12.toInt))
         linesScaled.foreach(println)
 
         val trainTest = linesEncoded.randomSplit(Array(0.8, 0.2)) // 80/20 train test split
